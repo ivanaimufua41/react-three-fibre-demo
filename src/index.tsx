1deae918fe08efe4123feab62a6d498d145cbb81
Ivan Aimufua <ivan.aimufua@punchcard.io>
2020-05-08 14:02:08 -0600

    current working project

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,120 @@
 import ReactDOM from 'react-dom';
 import * as React from 'react';
-import { Canvas } from 'react-three-fiber';
-import { Vector3 } from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { Canvas, extend, ReactThreeFiber, useThree, useFrame } from 'react-three-fiber';
+import * as THREE from 'three';
 
-import Box from './components/Box';
 import './indexStyle.css';
 
-const pointingLightPosition = new Vector3(10, 10, 10);
-const position1 = new Vector3(-1.2, 0, 0);
-const position2 = new Vector3(1.2, 0, 0);
+declare global {
+	namespace JSX {
+		interface IntrinsicElements {
+			orbitControls: ReactThreeFiber.Object3DNode<OrbitControls, typeof OrbitControls>
+			fog: ReactThreeFiber.Node<THREE.Fog, typeof THREE.Fog>
+		}
+	}
+}
+
+extend({ OrbitControls });
+
+const WaterBottle: React.FC = () => {
+	const [model, setModel] = React.useState<any>();
+	React.useEffect(() => {
+		new GLTFLoader().load('./scene.gltf', setModel);
+	}, []);
+
+	return model ? <primitive object={model.scene} /> : null;
+}
+
+const Plane = () => (
+	<mesh
+		rotation={[-Math.PI / 2, 0, 0]} // turnning 360
+		position={[0, -1, 0]}
+		receiveShadow={true}
+	>
+		<planeBufferGeometry
+			attach="geometry"
+			args={[100, 100]}
+		/>
+		<meshPhysicalMaterial
+			attach="material"
+			color='white'
+		/>
+	</mesh>
+)
+
+const Controls: React.FC = () => {
+	const { camera, gl } = useThree();
+	const orbitRef: any = React.useRef();
+
+	useFrame(() => orbitRef.current.update())
+	return (
+		<orbitControls
+			ref={orbitRef}
+			maxPolarAngle={Math.PI / 3}
+			minPolarAngle={Math.PI / 3}
+			autoRotate={true}
+			args={[camera, gl.domElement]}
+		/>
+	)
+}
+
+const Box: React.FC<{}> = () => {
+	// const meshRef: any = React.useRef();
+	const [hovered, setHovered] = React.useState(false);
+	const [active, setActive] = React.useState(false);
+
+	// useFrame(() => {
+	// 	meshRef.current.rotation.x = meshRef.current.rotation.y += 0.01
+	// })
+
+
+	return (
+		<mesh
+			// ref={meshRef}
+			onPointerOver={() => setHovered(true)}
+			onPointerOut={() => setHovered(false)}
+			onClick={() => setActive(!active)}
+			scale={active ? [3, 3, 3] : [1.1, 1.1, 1.1]}
+			castShadow={true}
+			receiveShadow={true}
+		>
+			<boxBufferGeometry
+				attach="geometry"
+				args={[1, 1, 1]}
+			/>
+			<meshPhysicalMaterial
+				attach="material"
+				color={hovered ? 'hotpink' : 'grey'}
+			/>
+		</mesh>
+	)
+}
+
+const colour = new THREE.Color("white");
 
 ReactDOM.render(
-    <Canvas colorManagement>
-      <ambientLight />
-      <pointLight position={pointingLightPosition} />
-      <Box position={position1} />
-      <Box position={position2} />
-    </Canvas>,
-    document.getElementById('root')
-  )
+	<Canvas
+		camera={{ position: [0, 0, 5] }}
+		shadowMap={true}
+	>
+		<fog
+			attach="fog"
+			args={[5, 10, 15]}
+			color={colour}
+		/>
+		<Controls />
+		<ambientLight castShadow={true}/>
+		<spotLight
+			position={[15, 20, 5]}
+			intensity={.3}
+			castShadow={true}
+			receiveShadow={true}
+		/>
+		<Box />
+		{/* <WaterBottle /> */}
+		<Plane />
+	</Canvas>,
+	document.getElementById('root')
+)
